d46bf924d1b8e925360cfcf3c8c303726ba4b063
mikemartincode <mikesdigitaldesign@gmail.com>
2018-04-18 01:17:40 -0400

    Downgraded some of the dependencies to fix HMR. Removed useless addons. Changed the create stories script to include addons.

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,12 +1,11 @@
 import React from "react";
 import { configure, addDecorator, setAddon } from "@storybook/react";
 import { BrowserRouter } from "react-router-dom";
-import centered from "@storybook/addon-centered";
-import { withKnobs } from "@storybook/addon-knobs/react";
 import { checkA11y } from "@storybook/addon-a11y";
 import { BackgroundColor } from "react-storybook-decorator-background";
-import { storiesOf } from "@storybook/react";
 import { setOptions } from "@storybook/addon-options";
+import centered from "@storybook/addon-centered";
+
 import JSXAddon from "storybook-addon-jsx";
 
 import "../src/style.less";
@@ -14,27 +13,18 @@ import "../src/style.less";
 setAddon(JSXAddon);
 
 setOptions({
-  name: "CRA Kitchen Sink",
-  goFullScreen: false,
-  showSearchBox: false,
-  sortStoriesByKind: true,
+  name: "AntDesign Theme Editor",
+  url: "https://github.com/minheq/antd-theme-editor",
+  sortStoriesByKind: false,
   hierarchySeparator: /\./,
-  hierarchyRootSeparator: /\|/,
-  enableShortcuts: false
+  hierarchyRootSeparator: /\|/
 });
 
 const antReq = require.context("../src/stories", true, /\.stories\.js$/);
-const componentReq = require.context(
-  "../src/components",
-  true,
-  /\.stories\.js$/
-);
-const viewReq = require.context("../src/views", true, /\.stories\.js$/);
 
 // global decorators
 addDecorator(checkA11y);
-addDecorator(withKnobs);
-addDecorator(centered);
+addDecorator(centered)
 addDecorator(story => <BrowserRouter>{story()}</BrowserRouter>);
 addDecorator(story => (
   <BackgroundColor colors={["#ffffff", "#000000"]} story={story} />
@@ -42,8 +32,6 @@ addDecorator(story => (
 
 function loadStories() {
   antReq.keys().forEach(filename => antReq(filename));
-  componentReq.keys().forEach(filename => componentReq(filename));
-  viewReq.keys().forEach(filename => viewReq(filename));
 }
 
 configure(loadStories, module);
--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,18 +1,21 @@
 // load the default config generator.
-const fs = require("fs");
-const path = require("path");
-const lessToJs = require("less-vars-to-js");
+const genDefaultConfig = require('@storybook/react/dist/server/config/defaults/webpack.config.js');
+const fs = require('fs');
+const path = require('path');
+const lessToJs = require('less-vars-to-js');
 
-module.exports = (baseConfig, env, defaultConfig) => {
+module.exports = (baseConfig, env) => {
+  const config = genDefaultConfig(baseConfig, env);
+
   // load custom variables for theme
-  defaultConfig.module.rules.push({
+  config.module.rules.push({
     test: /\.less$/,
     use: [
-      { loader: "style-loader" },
-      { loader: "css-loader" },
-      { loader: "less-loader", options: { javascriptEnabled: true } }
-    ]
+      { loader: 'style-loader' },
+      { loader: 'css-loader' },
+      { loader: 'less-loader' },
+    ],
   });
 
-  return defaultConfig;
+  return config;
 };
