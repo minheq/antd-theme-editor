ef521b4e42617eb2e88fd316dc1ab1943cfa5e58
Vu Hoang Minh <vuhminh@gmail.com>
2018-04-23 15:15:34 +0700

    add prettier command

--- scripts/exportFiles.js
+++ scripts/exportFiles.js
@@ -1,31 +1,34 @@
-const less = require('less');
-const path = require('path');
-const readFile = require('fs-readfile-promise');
-const writeFile = require('node-fs-writefile-promise');
-const fs = require('fs-extra');
+const less = require("less");
+const path = require("path");
+const readFile = require("fs-readfile-promise");
+const writeFile = require("node-fs-writefile-promise");
+const fs = require("fs-extra");
 
 const compileClasses = async () => {
   const lessInput = await readFile(
     path.resolve(__dirname, `../src/classes-override.less`),
-    'utf8'
+    "utf8"
   );
 
   const { css } = await less.render(lessInput, {
-    filename: path.resolve(__dirname, `../src/classes-override.less`),
+    filename: path.resolve(__dirname, `../src/classes-override.less`)
   });
 
-  await writeFile(path.resolve(__dirname, `../dist/antd-classes-override.css`), css);
+  await writeFile(
+    path.resolve(__dirname, `../dist/antd-classes-override.css`),
+    css
+  );
 
-  console.log('Compiled less classes');
+  console.log("Compiled less classes");
 };
 
 const copyVariablesOverride = async () => {
   await fs.copy(
-    path.resolve(__dirname, '../src/antd-theme-override.less'),
-    path.resolve(__dirname, '../dist/antd-theme-override.less')
+    path.resolve(__dirname, "../src/antd-theme-override.less"),
+    path.resolve(__dirname, "../dist/antd-theme-override.less")
   );
 
-  console.log('Copied antd theme override file');
+  console.log("Copied antd theme override file");
 };
 
 const exportFiles = async () => {
@@ -33,4 +36,4 @@ const exportFiles = async () => {
   await compileClasses();
 };
 
-exportFiles().then(() => console.log('Export finished'));
+exportFiles().then(() => console.log("Export finished"));
